925d76f9a3329fc3034bc564e65a12787d0f8c7a
Antoine R <wayzen.contact@gmail.com>
2024-03-30 15:44:58 +0100

    dev: :construction: continuing dev

---
+++ app/products/page.tsx
@@ -0,0 +1,13 @@
+import { DataTableDemo } from '@/components/data/data-table'
+import React from 'react'
+
+export default function ProductsPage() {
+  return (
+    <main>
+        <div className="container mx-auto">
+            <h1 className='text-4xl font-semibold mb-10'>Tous les produits</h1>
+            <DataTableDemo />
+        </div>
+    </main>
+  )
+}
