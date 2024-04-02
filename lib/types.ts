a7eda39342af00857148315276a373ae4588aea9
Antoine R <wayzen.contact@gmail.com>
2024-04-02 23:26:03 +0200

    fix: :label: fixing types

--- lib/types.ts
+++ lib/types.ts
@@ -1,68 +1,67 @@
-interface Product {
-    id: number;
-    name: string;
-    category?: ProductCategory;
-    categoryId?: number | null;
-    calories: number;
-    fat?: number | null;
-    fatSaturated?: number | null;
-    carbohydrates?: number | null;
-    sugar?: number | null;
-    fibres?: number | null;
-    salt?: number | null;
-  }
-  
-  interface RecipeStatus {
-    id: number;
-    name: string;
-    recipes: Recipe[];
-  }
-  
-  interface ProductCategory {
-    id: number;
-    name: string;
-    products: Product[];
-  }
-  
-  interface Recipe {
-    id: number;
-    name: string;
-    description: string | null;
-    steps: Step[];
-    duration?: number | null;
-    difficulty?: number | null;
-    totalCalories?: number | null;
-    totalProteins?: number | null;
-    status: RecipeStatus;
-    statusId: number;
-    products: ProductInRecipe[];
-    tags: string[];
-    category?: RecipeCategory | null;
-    categoryId?: number | null;
-    slug?: string | null;
-    link?: string | null;
-  }
-  
-  interface Step {
-    id: number;
-    recipe: Recipe;
-    recipeId: number;
-    order: number;
-    content: string;
-  }
-  
-  interface RecipeCategory {
-    id: number;
-    name: string;
-    recipes: Recipe[];
-  }
-  
-  interface ProductInRecipe {
-    id: number;
-    recipe: Recipe;
-    recipeId: number;
-    product: Product;
-    productId: number;
-    quantity: number;
-  }
-  
+export interface Product {
+  id: number;
+  name: string;
+  category?: ProductCategory | null;
+  categoryId?: number | null;
+  calories: number;
+  fat?: number | null;
+  fatSaturated?: number | null;
+  carbohydrates?: number | null;
+  sugar?: number | null;
+  fibres?: number | null;
+  salt?: number | null;
+}
+
+export interface RecipeStatus {
+  id: number;
+  name: string;
+  recipes: Recipe[];
+}
+
+export interface ProductCategory {
+  id: number;
+  name: string;
+  products?: Product[];
+}
+
+export interface Recipe {
+  id: number;
+  name: string;
+  description: string | null;
+  steps: Step[];
+  duration?: number | null;
+  difficulty?: number | null;
+  totalCalories?: number | null;
+  totalProteins?: number | null;
+  status: RecipeStatus;
+  statusId: number;
+  products: ProductInRecipe[];
+  tags: string[];
+  category?: RecipeCategory | null;
+  categoryId?: number | null;
+  slug?: string | null;
+  link?: string | null;
+}
+
+export interface Step {
+  id: number;
+  recipe: Recipe;
+  recipeId: number;
+  order: number;
+  content: string;
+}
+
+export interface RecipeCategory {
+  id: number;
+  name: string;
+  recipes: Recipe[];
+}
+
+export interface ProductInRecipe {
+  id: number;
+  recipe: Recipe;
+  recipeId: number;
+  product: Product;
+  productId: number;
+  quantity: number;
+}
